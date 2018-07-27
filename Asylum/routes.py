16efaeed7388661d45567e80faf40cca77e8f3b8
wurbanczyk <wojciechkamilurbanczyk@gmail.com>
2018-07-27 11:46:22 +0200

    Basic charts and carts on energy page

--- Asylum/routes.py
+++ Asylum/routes.py
@@ -1,37 +1,62 @@
 from flask import render_template
 from flask import jsonify
 import requests
+import json
+import datetime
 from . import models
 
 
 def init_routes(app):
-
     @app.route('/', methods=['GET'])
     @app.route('/home', methods=['GET'])
     def home():
-        model={
+        model = {
             'pageName': 'Strona Główna'
         }
         return render_template('home.html', model=model)
 
     @app.route('/energy', methods=['GET'])
     def energy():
-        powerProduction = models.EnergyProduction.query.order_by(models.EnergyProduction.id.desc()).first();
-        model={
-            'pageName': 'Energia'
+        power_production = models.EnergyProduction.query.order_by(models.EnergyProduction.id.desc()).limit(1440)\
+            .from_self().order_by(models.EnergyProduction.id).all()
+        energy_production = models.EnergyProduction.query.order_by(models.EnergyProduction.id.desc()).limit(11520) \
+            .from_self().order_by(models.EnergyProduction.id).all()
+        time = [datetime.datetime.fromtimestamp(o.time).strftime('%H:%M') for o in power_production]
+        power = [o.power for o in power_production]
+        energy = [o.energy for o in energy_production][1::1440]
+        energy = [(y - x)/1000 for x, y in zip(energy, energy[1:])]
+
+        print(energy)
+        model = {
+            'pageName': 'Energia',
+            'chartData': json.dumps(
+                {
+                    'labels': time[1::5],
+                    'datasets':
+                        [{
+                            'label': 'Test',
+                            'data': power[1::5],
+                            'borderWidth': 1,
+                            'backgroundColor': 'rgba(40, 255, 70, 0.5)'
+                        }]
+                }
+            ),
+            'currentPower': power[-1],
+            'energy': energy,
+            'maxPower': max(power)
         }
         return render_template('energy.html', model=model)
 
     @app.route('/blinds', methods=['GET'])
     def blinds():
-        model={
+        model = {
             'pageName': 'Rolety'
         }
         return render_template('blinds.html', model=model)
 
     @app.route('/locks', methods=['GET'])
     def locks():
-        model={
+        model = {
             'pageName': 'Zamki'
         }
         return render_template('locks.html', model=model)
---
+++ Asylum/static/js/pages/energy/energy-charts.js
@@ -0,0 +1,52 @@
+
+function genChart(data) {
+    Chart.defaults.global.elements.point.radius = 0;
+    var ctx = document.getElementById("power-today-chart").getContext('2d');
+    window.myChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            legend: {
+                display: false
+            },
+            maintainAspectRatio: false,
+            responsive: true,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes:[{
+                    ticks:{
+                        autoSkip: true,
+                        maxTicksLimit: 24
+                    }
+                }]
+            }
+        }
+    });
+
+    $('#productionPowerChart').peity('donut',{
+        fill: ["green", "#eeeeee"],
+        radius: 22,
+        innerRadius: 13
+    });
+     $('#productionPowerChart2').peity('bar',{
+        fill: ["purple"],
+         height: 40,
+         width: 80
+    });
+
+     $('#productionPowerChart3').peity('donut',{
+        fill: ["green", "#eeeeee"],
+        radius: 22,
+        innerRadius: 13
+    });
+     $('#productionPowerChart4').peity('donut',{
+        fill: ["green", "#eeeeee"],
+        radius: 22,
+        innerRadius: 13
+
+    });
+}
