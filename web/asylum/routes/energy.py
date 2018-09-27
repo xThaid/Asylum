7706049b5cda2498a01e021863a173d013d8ee08
Wojciech Urbańczyk <wojciechkamilurbanczyk@gmail.com>
2018-09-27 13:55:33 +0200

    Changed energy peity

--- web/asylum/routes/energy.py
+++ web/asylum/routes/energy.py
@@ -1,5 +1,6 @@
 from flask import render_template, jsonify, make_response
 from datetime import datetime, timedelta
+import time
 
 from asylum.core.page_model import PageModel
 from asylum.core.auth import authorize
@@ -16,20 +17,19 @@ def init_energy_routes(app):
             .add_breadcrumb_page('Energia', '/energy')\
             .to_dict()
 
-        days_count = 15
-
-        start_time = (datetime.now().replace(hour=0, minute=0, second=0) - timedelta(days=days_count - 1))
+        start_time = datetime.now().replace(hour=0, minute=0, second=0)
         data = Energy.get_last_rows(start_time.timestamp())
 
-        grouped_data = [[]]
-        curr_day = 0
-        next_time = start_time + timedelta(days=1)
+        grouped_data = []
+        for x in range(datetime.now().hour + 1):
+            grouped_data.append([])
+        curr_hour = 0
+        next_time = start_time + timedelta(hours=1)
         for entry in data:
-            if entry.time >= next_time.timestamp():
-                curr_day += 1
-                next_time = next_time + timedelta(days=1)
-                grouped_data.append([])
-            grouped_data[curr_day].append(entry)
+            while entry.time >= next_time.timestamp():
+                curr_hour += 1
+                next_time = next_time + timedelta(hours=1)
+            grouped_data[curr_hour].append(entry)
 
         productions = []
         imports = []
@@ -38,28 +38,53 @@ def init_energy_routes(app):
         uses = []
         storeds = []
         for group in grouped_data:
-            productions.append((group[-1].production - group[0].production) / 1000)
-            imports.append((group[-1].import_ - group[0].import_) / 1000)
-            exports.append((group[-1].export - group[0].export) / 1000)
-            consumptions.append(productions[-1] - exports[-1] + imports[-1])
-            uses.append(productions[-1] - exports[-1])
-            storeds.append(exports[-1] * 0.8 - imports[-1])
+            if len(group) == 0:
+                prod = imp = exp = 0
+            else:
+                prod = (group[-1].production - group[0].production) / 1000
+                imp = (group[-1].import_ - group[0].import_) / 1000
+                exp = (group[-1].export - group[0].export) / 1000
+
+            productions.append(prod)
+            imports.append(imp)
+            exports.append(exp)
+
+            consumptions.append(prod - exp + imp)
+            uses.append(prod - exp)
+            storeds.append(exp * 0.8 - imp)
 
         time_from_midnight = datetime.now().timestamp() - datetime.now().replace(hour=0, minute=0, second=0).timestamp()
         if time_from_midnight == 0:
             time_from_midnight = 1
 
+        if len(data) == 0:
+            production_delta = import_delta = export_delta = 0
+        else:
+            production_delta = data[-1].production - data[0].production
+            import_delta = data[-1].import_ - data[0].import_
+            export_delta = data[-1].export - data[0].export
+
+        consumption_delta = production_delta - export_delta + import_delta
+        use_delta = production_delta - export_delta
+        stored_delta = export_delta * 0.8 - import_delta
+
         data_model = {
-            'power_production_average': int(productions[-1] * 1000 * 3600 / time_from_midnight),
+            'power_production_average': int(production_delta * 3600 / time_from_midnight),
             'power_production_max': 1,
             'energy_production': productions,
-            'power_consumption_average': int(consumptions[-1] * 1000 * 3600 / time_from_midnight),
+            'energy_production_total': production_delta / 1000,
+            'power_consumption_average': int(consumption_delta * 3600 / time_from_midnight),
             'power_consumption_max': 1,
             'energy_consumption': consumptions,
+            'energy_consumption_total': consumption_delta / 1000,
             'energy_use': uses,
+            'energy_use_total': use_delta / 1000,
             'energy_import': imports,
+            'energy_import_total': import_delta / 1000,
             'energy_export': exports,
-            'energy_stored': storeds
+            'energy_export_total': export_delta / 1000,
+            'energy_stored': storeds,
+            'energy_stored_total': stored_delta / 1000
         }
 
         return render_template('energy.html', page_model=page_model, data_model=data_model)
