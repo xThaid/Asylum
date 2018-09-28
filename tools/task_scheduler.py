e57f757ded9341d8e236cc99db73cffea0bcc85a
Jakub Urbańczyk <xthaid@gmail.com>
2018-09-28 11:33:40 +0200

    Fix task scheduler

--- tools/task_scheduler.py
+++ tools/task_scheduler.py
@@ -1,46 +1,61 @@
-from astral import Location
-from datetime import datetime
-
+import datetime
 import config
 import db
+
+from astral import Location
+
+SCHEDULE_TASK_TIMEOUT = 60 * 30
 
 cfg = config.config
 
 loc = Location()
 loc.latitude = float(cfg['LOCATION']['Latitude'])
 loc.longitude = float(cfg['LOCATION']['Longitude'])
-sun = loc.sun()
+
+
+def calcHours(day):
+    hours = {}
+
+    sun = loc.sun(date=day)
+    hours[1] = sun['dawn']
+    hours[2] = sun['sunrise']
+    hours[3] = sun['noon']
+    hours[4] = sun['sunset']
+    hours[5] = sun['dusk']
+    hours[6] = datetime.datetime.combine(day, datetime.datetime.min.time())
+    return hours
+
+
+today = datetime.date.today()
+todayHours = calcHours(today)
+tommorowHours = calcHours(today + datetime.timedelta(days=1))
 
 conn = db.create_connection()
 try:
     cur = conn.cursor()
     cur.execute("SELECT * FROM blinds_schedule")
 
-    rows = cur.fetchall()
+    schedules = cur.fetchall()
 
-    for row in rows:
+    for sched in schedules:
+        sched_id, device_id, action_id, hour_type, time_offset = sched
+
         task = cur.execute("SELECT * FROM blinds_task WHERE schedule_id = ?",
-                           (row[0],)).fetchall()
+                           (sched_id)).fetchall()
         if not task:
-            time = 0
-            now = datetime.now()
-            if row[3] == 0:
-                time = now.replace(hour=0, minute=0, second=0, microsecond=0) \
-                    .timestamp() + row[4]
-            elif row[3] == 1:
-                time = sun['dawn'].timestamp() + row[4]
-            elif row[3] == 2:
-                time = sun['sunrise'].timestamp() + row[4]
-            elif row[3] == 3:
-                time = sun['sunset'].timestamp() + row[4]
-            elif row[3] == 4:
-                time = sun['dusk'].timestamp() + row[4]
+            offset = datetime.timedelta(seconds=time_offset)
+            time = todayHours[hour_type] + offset
 
-            if time < now.timestamp():
-                time += 86400
+            if time < datetime.datetime.now():
+                time = tommorowHours[hour_type] + offset
 
             cur.execute("INSERT INTO blinds_task (time, device, action, schedule_id, timeout) \
-                VALUES (?, ?, ?, ?, ?)", (int(time), row[1], row[2], row[0], 30 * 60))
+                VALUES (?, ?, ?, ?, ?)", (
+                int(time.timestamp()),
+                device_id,
+                action_id,
+                sched_id,
+                SCHEDULE_TASK_TIMEOUT))
 finally:
     conn.commit()
     conn.close()
