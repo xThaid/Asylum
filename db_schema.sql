71981e126fbb638ffab3e0ea29111aeaae001388
Wojciech Urbańczyk <wojciechkamilurbanczyk@gmail.com>
2019-01-04 13:35:20 +0100

    Added meteo and meteo_daily tables

--- db_schema.sql
+++ db_schema.sql
@@ -1,10 +1,14 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user_mac_address;
 DROP TABLE IF EXISTS energy;
+DROP TABLE IF EXISTS energy_daily;
 DROP TABLE IF EXISTS blinds_task;
 DROP TABLE IF EXISTS blinds_task_history;
 DROP TABLE IF EXISTS blinds_schedule;
+DROP TABLE IF EXISTS meteo;
+DROP TABLE IF EXISTS meteo_daily
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,6 +36,23 @@ CREATE TABLE energy(
   power_export INTEGER NOT NULL,
 
   CONSTRAINT time_unique UNIQUE (time)
+);
+
+CREATE TABLE energy_daily(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  day_ordinal INTEGER NOT NULL,
+  production INTEGER NOT NULL,
+  import INTEGER NOT NULL,
+  export INTEGER NOT NULL,
+  production_offset INTEGER NOT NULL,
+  import_offset INTEGER NOT NULL,
+  export_offset INTEGER NOT NULL,
+  max_power_production INTEGER NOT NULL,
+  max_power_import INTEGER NOT NULL,
+  max_power_export INTEGER NOT NULL,
+  max_power_consumption INTEGER NOT NULL,
+  max_power_use INTEGER NOT NULL,
+  max_power_store INTEGER NOT NULL
 );
 
 CREATE TABLE blinds_task(
@@ -68,21 +89,40 @@ CREATE TABLE blinds_schedule(
   FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
-CREATE TABLE energy_daily(
+CREATE TABLE meteo(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time INTEGER NOT NULL,
+  temperature INTEGER NOT NULL,
+  humidity INTEGER NOT NULL,
+  pressure INTEGER NOT NULL,
+  dust_PM10 INTEGER NOT NULL,
+  dust_PM25 INTEGER NOT NULL,
+  dust_PM100 INTEGER NOT NULL,
+
+  CONSTRAINT time_unique UNIQUE (time)
+);
+
+CREATE TABLE meteo_daily(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   day_ordinal INTEGER NOT NULL,
-  production INTEGER NOT NULL,
-  import INTEGER NOT NULL,
-  export INTEGER NOT NULL,
-  production_offset INTEGER NOT NULL,
-  import_offset INTEGER NOT NULL,
-  export_offset INTEGER NOT NULL,
-  max_power_production INTEGER NOT NULL,
-  max_power_import INTEGER NOT NULL,
-  max_power_export INTEGER NOT NULL,
-  max_power_consumption INTEGER NOT NULL,
-  max_power_use INTEGER NOT NULL,
-  max_power_store INTEGER NOT NULL
+  temperature_min INTEGER NOT NULL,
+  temperature_avg INTEGER NOT NULL,
+  temperature_max INTEGER NOT NULL,
+  humidity_min INTEGER NOT NULL,
+  humidity_avg INTEGER NOT NULL,
+  humidity_max INTEGER NOT NULL,
+  pressure_min INTEGER NOT NULL,
+  pressure_avg INTEGER NOT NULL,
+  pressure_max INTEGER NOT NULL,
+  dust_PM10_min INTEGER NOT NULL,
+  dust_PM10_avg INTEGER NOT NULL,
+  dust_PM10_max INTEGER NOT NULL,
+  dust_PM25_min INTEGER NOT NULL,
+  dust_PM25_avg INTEGER NOT NULL,
+  dust_PM25_max INTEGER NOT NULL,
+  dust_PM100_min INTEGER NOT NULL,
+  dust_PM100_avg INTEGER NOT NULL,
+  dust_PM100_max INTEGER NOT NULL
 );
 
 COMMIT;
