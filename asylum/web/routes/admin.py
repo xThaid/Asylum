6cbc0065a058be268e979a8fbf6da13263dbbd0b
Wojciech Urbańczyk <wojciechkamilurbanczyk@gmail.com>
2019-01-05 19:30:03 +0100

    Added access to devices by website

--- asylum/web/routes/admin.py
+++ asylum/web/routes/admin.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, jsonify
+import requests
 
 from asylum.web.core.page_model import PageModel
 from asylum.web.core.auth import authorize
 from asylum.energy import energy_data
+from asylum.meteo import meteo_data
+from asylum import config
 
 
 def init_auth_routes(app):
@@ -22,6 +25,17 @@ def init_auth_routes(app):
             .to_dict()
         data_model = {
             'flara_status': energy_data.test_flara_connection(),
-            'emeter_status': energy_data.test_emeter_connection()
+            'emeter_status': energy_data.test_emeter_connection(),
+            'meteo_status': meteo_data.test_meteo_connection()
         }
         return render_template('admin/status.html', data_model=data_model, page_model=page_model)
+
+    @app.route('/admin/status/meteo', methods=['GET'])
+    @authorize('admin')
+    def status_meteo(context):
+        return jsonify(requests.get(config['SUBSYSTEMS']['meteo_url'], timeout=3).json())
+
+    @app.route('/admin/status/emeter', methods=['GET'])
+    @authorize('admin')
+    def status_emeter(context):
+        return jsonify(requests.get(config['SUBSYSTEMS']['emeter_url'], timeout=3).json())
