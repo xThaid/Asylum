dbc54cb701607438a4f6ec62116f4d2b6938c052
Jakub Urbańczyk <xthaid@gmail.com>
2019-01-05 14:37:15 +0100

    add formula for relative pressure

--- asylum/meteo/meteo_data.py
+++ asylum/meteo/meteo_data.py
@@ -2,16 +2,20 @@ import requests
 
 from asylum import config
 
+ALTITUDE = 233
+
 
 def get_data():
     try:
-        meteo_data = requests.get(config['SUBSYSTEMS']['meteo_url'], timeout=1).json()
+        meteo_data = requests.get(config['SUBSYSTEMS']['meteo_url'], timeout=3).json()
+
+        relative_pressure = calc_relative_pressure(int(meteo_data['bmppress']), int(meteo_data['sitemp']) / 10)
 
         return {
             'cold_device': meteo_data['cold'],
             'temperature': meteo_data['sitemp'],
             'humidity': meteo_data['sihumidity'],
-            'pressure': meteo_data['bmppress'],
+            'pressure': relative_pressure,
             'dust_PM10': meteo_data['pms3'],
             'dust_PM25': meteo_data['pms4'],
             'dust_PM100': meteo_data['pms5']
@@ -23,3 +27,10 @@ def get_data():
 def test_meteo_connection():
     data = get_data()
     return data is not None
+
+
+def calc_relative_pressure(pressure, temperature):
+    f1 = 0.0065 * ALTITUDE
+    f2 = 1.0 - (f1 / (f1 + temperature + 273.15))
+    f3 = pow(f2, -5.257)
+    return int(pressure * f3)
